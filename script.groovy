a01ca4894dd00ef74c6aeaaae8a9b60b59eff859
Alexey Kiyashkin <akiyashkin@gmail.com>
2023-07-17 18:46:06 +0300

    adding groovy script

--- script.groovy
+++ script.groovy
@@ -2,9 +2,9 @@ def buildImage(imagename, imageversion) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: '6ec9d130-1d94-42af-9cf5-26ab651d58da', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             sh """
-                docker build ./frontend -t kryssperer/$imagename:$imageversion
+                docker build . -t kryssperer/${imagename}:${imageversion}
                 echo $PASS | docker login -u $USER --password-stdin
-                docker push kryssperer/imagename:$imageversion
+                docker push kryssperer/${imagename}:${imageversion}
             """
     }
-} 
+}
